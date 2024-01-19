ed99e539082870275f4505e8bd80f978d0298e4d
thommorais <thommorais@gmail.com>
2024-01-19 01:38:08 +0000

    18 Jan 2024, 22:38

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,8 +1,10 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 import { forwardRef, HTMLAttributes } from 'react'
+import { tv } from 'tailwind-variants'
 
 const alertVariants = tv({
+	base: 'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
 	variants: {
 		variant: {
 			default: 'bg-background text-foreground',
