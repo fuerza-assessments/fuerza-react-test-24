668c22670bcb8b957870dd3de6299e81a3b97532
thommorais <thommorais@gmail.com>
2024-01-19 01:19:18 +0000

    18 Jan 2024, 22:19

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { AuthOptions, DefaultSession, getServerSession } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 import Auth0Provider from 'next-auth/providers/auth0'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 import { env } from '@/env'
 
@@ -22,6 +23,29 @@ export const authConfig = {
 		strategy: 'jwt',
 	},
 	providers: [
+		CredentialsProvider({
+			// The name to display on the sign in form (e.g. "Sign in with...")
+			name: 'Credentials',
+			// `credentials` is used to generate a form on the sign in page.
+			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
+			// e.g. domain, username, password, 2FA token, etc.
+			// You can pass any HTML attribute to the <input> tag through the object.
+			credentials: {
+				username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials, req) {
+				// Add logic here to look up the user from the credentials supplied
+				const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+
+				if (user) {
+					// Any object returned will be saved in `user` property of the JWT
+					return user
+				}
+				// If you return null then an error will be displayed advising the user to check their details.
+				return null
+			},
+		}),
 		GoogleProvider({
 			clientId: env.GOOGLE_ID,
 			clientSecret: env.GOOGLE_SECRET,
