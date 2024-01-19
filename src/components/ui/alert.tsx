832de20156558d5df5a6f4dd5fb8fc38cc3f3dfb
thommorais <thommorais@gmail.com>
2024-01-19 01:37:59 +0000

    18 Jan 2024, 22:37

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -2,20 +2,17 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 import { forwardRef, HTMLAttributes } from 'react'
 
-const alertVariants = cva(
-	'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
-	{
-		variants: {
-			variant: {
-				default: 'bg-background text-foreground',
-				destructive: 'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
-			},
-		},
-		defaultVariants: {
-			variant: 'default',
+const alertVariants = tv({
+	variants: {
+		variant: {
+			default: 'bg-background text-foreground',
+			destructive: 'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
 		},
 	},
-)
+	defaultVariants: {
+		variant: 'default',
+	},
+})
 
 const Alert = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>>(({ className, variant, ...props }, ref) => (
 	<div ref={ref} role='alert' className={cn(alertVariants({ variant }), className)} {...props} />
