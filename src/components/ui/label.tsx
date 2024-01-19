758af81209c6bca1839244253dceacf493c11a6f
thommorais <thommorais@gmail.com>
2024-01-19 01:40:11 +0000

    18 Jan 2024, 22:40

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -6,7 +6,9 @@ import { tv, type VariantProps } from 'tailwind-variants'
 
 import { cn } from '@/lib/utils'
 
-const labelVariants = tv('text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70')
+const labelVariants = tv({
+	base: 'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+})
 
 const Label = forwardRef<ElementRef<typeof LabelPrimitive.Root>, ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>>(
 	({ className, ...props }, ref) => <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />,
