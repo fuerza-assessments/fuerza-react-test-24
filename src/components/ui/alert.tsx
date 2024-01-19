95182e5509d874ab9559731dd6f1393e5d83a979
thommorais <thommorais@gmail.com>
2024-01-19 03:11:01 +0000

    19 Jan 2024, 00:11

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -29,6 +29,7 @@ AlertTitle.displayName = 'AlertTitle'
 const AlertDescription = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(({ className, ...props }, ref) => (
 	<div ref={ref} className={cn('text-sm [&_p]:leading-relaxed', className)} {...props} />
 ))
+
 AlertDescription.displayName = 'AlertDescription'
 
 export { Alert, AlertTitle, AlertDescription }
