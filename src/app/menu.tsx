2d9fe3a0127ba1b6be822ed9b6e3c71080c5575d
thommorais <thommorais@gmail.com>
2024-01-19 01:32:58 +0000

    18 Jan 2024, 22:32

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -5,6 +5,7 @@ export const Menu = () => {
 	const pathname = usePathname()
 	return (
 		<nav className='ml-8 flex space-x-4'>
+			{pathname}
 			<Link href='/dashboard'>stock</Link>
 			<Link href='/dashboard/tier-list'>tier-list</Link>
 		</nav>
