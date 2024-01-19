aa00b77c232de87cfd4d58c3aa462d067089052a
thommorais <thommorais@gmail.com>
2024-01-19 00:45:47 +0000

    18 Jan 2024, 21:45

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'drizzle-kit'
-import { env } from '@/env'
+import type { Config } from "drizzle-kit";
+import { env } from "@/env";
 
 export default {
-	schema: './src/db/schema.ts',
-	out: './drizzle/migrations',
-	driver: 'pg',
+	schema: "./src/db/schema.ts",
+	out: "./drizzle/migrations",
+	driver: "pg",
 	dbCredentials: {
 		connectionString: env.DATABASE_URL,
 	},
-} satisfies Config
+} satisfies Config;
