2db5660a2ffa7e87ea0504ed3cd58f9558928492
thommorais <thommorais@gmail.com>
2024-01-19 01:31:01 +0000

    18 Jan 2024, 22:31

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-const MenuComponent = () => {
+export const Menu = () => {
 	return (
 		<nav className='ml-8 flex space-x-4'>
 			<Link href='/dashboard'>stock</Link>
