fae7379f3693699d63c3ba4e9885bfc616c7b4cf
thommorais <thommorais@gmail.com>
2024-01-19 01:34:26 +0000

    18 Jan 2024, 22:34

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -1,14 +1,23 @@
 'use client'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import { cva } from 'class-variance-authority'
+
+const currentMenu = cva({
+	base: 'text-red-fuerza',
+})
 
 export const Menu = () => {
 	const pathname = usePathname()
 	return (
 		<nav className='ml-8 flex space-x-4'>
 			{pathname}
-			<Link href='/dashboard'>stock</Link>
-			<Link href='/dashboard/tier-list'>tier-list</Link>
+			<Link className={currentMenu()} href='/dashboard'>
+				stock
+			</Link>
+			<Link className={currentMenu()} href='/dashboard/tier-list'>
+				tier-list
+			</Link>
 		</nav>
 	)
 }
